1afd4ec72cd59183f331cab40ea5878efb20efed
xutengfei <18515219314@163.com>
2018-04-20 16:08:26 +0800

    city router

---
+++ src/pages/city/City.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div>
+    <city-header></city-header>
+  </div>
+</template>
+
+<script>
+import CityHeader from './components/Header.vue'
+export default {
+  name: 'City',
+  components: {
+    CityHeader
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
