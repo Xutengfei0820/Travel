f7d342658377f42b721a0137571b48615b78f8fb
xutengfei <18515219314@163.com>
2018-04-22 00:36:28 +0800

    banner is done

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,74 @@
+<template lang="html">
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg" alt="">
+      <div class="banner-info">
+        <div class="banner-tittle">故宫(AAAAA景区)</div>
+        <div class="banner-number">
+          <span class="iconfont">&#xe6dd;</span>10
+        </div>
+      </div>
+    </div>
+    <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary.vue'
+export default {
+  name: 'Detail',
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg',
+        'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg']
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  },
+  components: {
+    CommonGallary
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    color: #fff
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #fff
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, .8))
+      .banner-tittle
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-number
+        height: .32rem
+        margin-top: .14rem
+        line-height: .32rem
+        padding: 0 .4rem
+        border-radius: .2rem
+        background: rgba(0, 0, 0, .8)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+
+</style>
